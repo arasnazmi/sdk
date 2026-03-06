5981ccb741418004bd76370ebf9580f270b2bc10
Nazmi Aras <nazmi.aras@t3gemstone.org>
2026-03-06 17:27:38 +0300

    fixed

--- distro/scripts/t3-gem-o1/install-local-apt.sh
+++ distro/scripts/t3-gem-o1/install-local-apt.sh
@@ -65,7 +65,6 @@ apt-get install -y \
     kernel-module-rpi-panel-attiny-regulator \
     kernel-module-rpmsg-char \
     kernel-module-rpmsg-ctrl \
-    kernel-module-rpmsg-ns \
     kernel-module-snd-usb-audio \
     kernel-module-spidev \
     kernel-module-tc358762 \
@@ -74,17 +73,6 @@ apt-get install -y \
     kernel-module-ti-usb-3410-5052 \
     kernel-module-ums-usbat \
     kernel-module-usb-conn-gpio \
-    kernel-module-usb-f-acm \
-    kernel-module-usb-f-ecm \
-    kernel-module-usb-f-ecm-subset \
-    kernel-module-usb-f-eem \
-    kernel-module-usb-f-fs \
-    kernel-module-usb-f-hid \
-    kernel-module-usb-f-mass-storage \
-    kernel-module-usb-f-ncm \
-    kernel-module-usb-f-rndis \
-    kernel-module-usb-f-serial \
-    kernel-module-usb-f-ss-lb \
     kernel-module-usb-f-uac1 \
     kernel-module-usb-f-uac2 \
     kernel-module-usb-serial-simple \
