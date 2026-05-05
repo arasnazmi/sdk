687d20fd26708fef6c755f333775546c0c690af2
gemstonedevteam <dev@t3gemstone.org>
2026-05-05 15:39:34 +0300

    Add share-network script for internet sharing configuration

---
+++ distro/scripts/custom/share-network.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Host
+# $ ./share-network.sh wlp0s20f3 enx02123456789a
+
+# Target
+# $ sudo ip route add default via 192.168.7.59 dev usb0
+
+INTERNET_IFACE="${1:-wlp0s20f3}"
+OTHER_IFACE="${2:-enx02123456789a}"
+
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+sudo iptables -A FORWARD -i $OTHER_IFACE -o $INTERNET_IFACE -j ACCEPT
+sudo iptables -A FORWARD -i $INTERNET_IFACE -o $OTHER_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o $INTERNET_IFACE -j MASQUERADE
