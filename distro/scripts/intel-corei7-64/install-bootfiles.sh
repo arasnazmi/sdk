cb74b18ef278dfbdae353a2602d5475313301210
gemstonedevteam <dev@t3gemstone.org>
2026-05-06 09:45:16 +0300

    Update deployment directory to use musl instead of glibc for Intel Core i7-64

--- distro/scripts/intel-corei7-64/install-bootfiles.sh
+++ distro/scripts/intel-corei7-64/install-bootfiles.sh
@@ -6,7 +6,7 @@ WORKDIR="$1"
 ROOTDIR="$2"
 IMAGEPATH="$3"
 
-DIR_IMGS="$WORKDIR/build/tmp-glibc/deploy/images/intel-corei7-64"
+DIR_IMGS="$WORKDIR/build/tmp-musl/deploy/images/intel-corei7-64"
 
 mkdir -p "$ROOTDIR/boot"
 cp "$DIR_IMGS/bzImage-intel-corei7-64.bin" "$ROOTDIR/boot/bzImage"
